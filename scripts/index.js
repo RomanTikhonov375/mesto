0212ac58cf3d8decfb880b1def232f0eb48d17c8
Roman Tikhonov <Roman.Tikhonov375@gmail.com>
2023-06-12 01:23:06 +0300

    bugfix

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,20 @@
 const page = document.querySelector('.page');
-const editButton = page.querySelector('.edit-button');
-const closeButton = page.querySelector('.form-edit-profile__close-btn');
+const editButton = page.querySelector('.profile__edit-button');
+const closeButton = page.querySelector('.popup__close-btn');
 const popup = page.querySelector('.popup');
-const formEditProfile = page.querySelector('.form-edit-profile');
-const likeButton = page.querySelectorAll('.like-button');
-
+const formEditProfile = page.querySelector('.popup__form-edit-profile');
+const likeButton = page.querySelectorAll('.card__like-button');
+// Функция обработчик клика 
 function handleClick () {
     popup.classList.toggle('popup_opened');
 }
-
+// Добавление функции кнопкам редактировать пользователя и закрыть форму
 editButton.addEventListener('click', handleClick);
 closeButton.addEventListener('click',handleClick);
-
+// Передаем значения со страницы в форму
 formEditProfile[0].value = page.querySelector('.profile__user-name').innerText;
 formEditProfile[1].value = page.querySelector('.profile__career').innerText;
-
+// Функция обработчик отправки формы
 function handleFormSubmit (evt) {
     evt.preventDefault();
     let profileUserName = page.querySelector('.profile__user-name');
@@ -30,7 +30,7 @@ formEditProfile.addEventListener('submit', handleFormSubmit);
 
 for (let i = 0; i < likeButton.length; i++) {
     function activeLikeButton () {
-        likeButton[i].classList.toggle('like-button_active');
+        likeButton[i].classList.toggle('card__like-button_active');
     }
     likeButton[i].addEventListener('click', activeLikeButton);
 }
